9d0472fc6b87028cf0259784add63e81e4698f78
wds-win11 <wdsnpshy@163.com>
2024-10-10 02:53:26 +0800

    底部状态栏显示完成

---
+++ pages/directory/directory.js
@@ -0,0 +1,12 @@
+Page({
+  data: {
+    items: [
+      { name: '项目1', description: '这是项目1的描述' },
+      { name: '项目2', description: '这是项目2的描述' },
+      { name: 'dd1', description: '这是项目3的描述' }
+    ]
+  },
+  onLoad() {
+    // 页面加载时执行
+  }
+})
